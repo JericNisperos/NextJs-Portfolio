c06a37229ac2f3cc93ef3ddce13e1ce288fa4fe3
Jeric <jericrnisperos@gmail.com>
2023-04-25 22:41:49 +0800

    fixed some issues again and again with cursor

--- nextjs-portfolio/src/pages/components/CustomCursor.js
+++ nextjs-portfolio/src/pages/components/CustomCursor.js
@@ -1,22 +1,22 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion, useSpring } from "framer-motion";
 import useCursor from "../hooks/CustomCursorHook";
 
 const CustomCursor = () => {
-    const cursorPos = useCursor();
-  
-    return (
-      <motion.div
-        className="cursor"
-        animate={{ x: cursorPos[0], y: cursorPos[1] }}
-        transition={{ duration: 0 }}
-      >
-        <motion.div 
-        animate={{rotate: 360}}
-        transition={{duration: 5, repeat: Infinity}}
-        
-        className="circle hidden md:block invert dark:invert-0 bg-gradient-to-bl from-orange-600 via-purple-600 to-blue-600" />
-      </motion.div>
-    );
-  };
+  const cursorPos = useCursor();
 
-  export default CustomCursor;
+  const cursorSpring = useSpring(cursorPos, { stiffness: 50, damping: 10 });
+
+
+  return (
+    <AnimatePresence>
+      <motion.div className="cursor" animate={{ x: cursorPos[0], y: cursorPos[1] }} transition={{ duration: 0 }}>
+        <motion.div animate={{ rotate: 360 }} transition={{ duration: 5, repeat: Infinity }} className="circle hidden md:block invert dark:invert-0 bg-gradient-to-bl from-orange-600 via-purple-600 to-blue-600 w-[16px] h-[16px]" />
+      </motion.div>
+      {/* <motion.div className="cursor" animate={{ x: cursorPos[0], y: cursorPos[1] }} transition={{ duration: 0.1, restSpeed: 0.5 }}>
+        <motion.div animate={{ rotate: 360 }} transition={{ duration: 5, repeat: Infinity }} className=" hidden md:block border-4 border-red-500 w-[50px] h-[50px]" />
+      </motion.div> */}
+    </AnimatePresence>
+  );
+};
+
+export default CustomCursor;
