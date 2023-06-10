affeb2051760862f772a8ecef31c41dd6a8b676e
Jeric <jericrnisperos@gmail.com>
2023-06-10 16:32:20 +0800

    add target to link items

--- nextjs-portfolio/src/pages/components/LinkItems.js
+++ nextjs-portfolio/src/pages/components/LinkItems.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 function LinkItems({ href, icon }) {
 
   return (
-    <Link href={href ?? ''}>
+    <Link href={href ?? ''}target="_blank">
     <motion.button whileHover={{ scale: 1.5 }} whileTap={{ scale: 1.2 }} className="dark:text-black text-white mx-4 px-4 py-2 rounded-full dark:bg-blue-400 bg-cyan-400 hover:dark:bg-cyan-500 hover:bg-cyan-300" target="_blank">
         <FontAwesomeIcon icon={icon} />
     </motion.button>
